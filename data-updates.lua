1da0f5b787480e52d0089ad888ab0b29a13981e9
TheTaurunti <thetaurunti@gmail.com>
2024-10-28 16:42:30 -0700

    update for 2.0

--- data-updates.lua
+++ data-updates.lua
@@ -8,37 +8,37 @@ local items = data.raw["item"]
 local recipes = data.raw["recipe"]
 local fluids = data.raw["fluid"]
 
-local fluid_barrels = {}
 
 -- =========================
 -- Defining helper functions
 -- =========================
 
 local function get_recipe_result(recipe)
-  local recipe_standard = recipe.normal or recipe
-  return (
-    recipe_standard.results and recipe_standard.results[1] and (
-      recipe_standard.results[1].name
-      or recipe_standard.results[1][1]
-    )
-  ) or recipe_standard.result
+	local recipe_standard = recipe.normal or recipe
+	return (
+		recipe_standard.results and recipe_standard.results[1] and (
+			recipe_standard.results[1].name
+			or recipe_standard.results[1][1]
+		)
+	) or recipe_standard.result
 end
 
 local function get_fluid(fluid_name)
-  for _, fluid in pairs(fluids) do
-    if (fluid.name == fluid_name)
-    then
-      return fluid
-    end
-  end
+	for _, fluid in pairs(fluids) do
+		if (fluid.name == fluid_name)
+		then
+			return fluid
+		end
+	end
 end
 
 -- =========================
 -- Putting together the data
 -- =========================
 
+local fluid_barrels = {}
 NON_FLUID_BARRELS = {
-  ["empty-barrel"] = true
+	["empty-barrel"] = true
 }
 
 -- Compat
@@ -47,48 +47,48 @@ require("compatibility.data-updates.dirty-fluid-containers")
 
 -- Stage 1
 for _, item in pairs(items) do
-  if (string.find(item.name, "-barrel") and not NON_FLUID_BARRELS[item.name])
-  then
-    fluid_barrels[item.name] = {
-      item_prototype = item,
-      recipe_fill = nil,
-      recipe_empty = nil,
-      fluid = nil
-    }
-  end
+	if (string.find(item.name, "-barrel") and not NON_FLUID_BARRELS[item.name])
+	then
+		fluid_barrels[item.name] = {
+			item_prototype = item,
+			recipe_fill = nil,
+			recipe_empty = nil,
+			fluid = nil
+		}
+	end
 end
 
 -- Stage 2
 for barrel_name, data_table in pairs(fluid_barrels) do
-  local fill_recipe_name = "fill-" .. barrel_name
-  local empty_recipe_name = "empty-" .. barrel_name
+	local fill_recipe_name = barrel_name
+	local empty_recipe_name = "empty-" .. barrel_name
 
-  if (recipes[fill_recipe_name])
-  then
-    data_table.recipe_fill = recipes[fill_recipe_name]
-  end
+	if (recipes[fill_recipe_name])
+	then
+		data_table.recipe_fill = recipes[fill_recipe_name]
+	end
 
-  if (recipes[empty_recipe_name])
-  then
-    data_table.recipe_empty = recipes[empty_recipe_name]
+	if (recipes[empty_recipe_name])
+	then
+		data_table.recipe_empty = recipes[empty_recipe_name]
 
-    local fluid_name = get_recipe_result(recipes[empty_recipe_name])
-    local fluid_prototype = get_fluid(fluid_name)
-    data_table.fluid = fluid_prototype
-  end
+		local fluid_name = get_recipe_result(recipes[empty_recipe_name])
+		local fluid_prototype = get_fluid(fluid_name)
+		data_table.fluid = fluid_prototype
+	end
 end
 
 local function get_first_icon(prototype_base)
-  if (prototype_base.icons)
-  then
-    return prototype_base.icons[1]
-  else
-    return {
-      icon = prototype_base.icon,
-      icon_size = prototype_base.icon_size,
-      icon_mipmaps = prototype_base.icon_mipmaps
-    }
-  end
+	if (prototype_base.icons)
+	then
+		return prototype_base.icons[1]
+	else
+		return {
+			icon = prototype_base.icon,
+			icon_size = prototype_base.icon_size,
+			icon_mipmaps = prototype_base.icon_mipmaps
+		}
+	end
 end
 
 -- ===============================================
@@ -97,21 +97,21 @@ end
 
 -- set icons based on fluid prototype icon, and the actual recipe/barrel icon.
 for _, data_table in pairs(fluid_barrels) do
-  local item_prototype = data_table.item_prototype
-  local empty_barrel = data.raw["item"]["empty-barrel"]
+	local item_prototype = data_table.item_prototype
+	local empty_barrel = data.raw["item"]["empty-barrel"]
 
-  if (MAKE_FLUID_ICON_PRIMARY and empty_barrel)
-  then
-    local fluid_icon = get_first_icon(data_table.fluid)
-    local barrel_icon = get_first_icon(empty_barrel)
-    barrel_icon.scale = 0.25
-    barrel_icon.shift = { -8, -8 }
+	if (MAKE_FLUID_ICON_PRIMARY and empty_barrel)
+	then
+		local fluid_icon = get_first_icon(data_table.fluid)
+		local barrel_icon = get_first_icon(empty_barrel)
+		barrel_icon.scale = 0.25
+		barrel_icon.shift = { -8, -8 }
 
-    item_prototype.icons = {
-      fluid_icon,
-      barrel_icon
-    }
-  else
-    item_prototype.icons = table.deepcopy(data_table.recipe_fill.icons)
-  end
+		item_prototype.icons = {
+			fluid_icon,
+			barrel_icon
+		}
+	else
+		item_prototype.icons = table.deepcopy(data_table.recipe_fill.icons)
+	end
 end
